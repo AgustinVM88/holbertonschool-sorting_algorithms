2d3bc3715a19f29630d0c67e685b136532909f60
Dyasteek <lawarsenik@gmail.com>
2025-08-10 22:09:45 -0300

    Betty improved

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,9 +12,8 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp, *actual, *change;
 
 	if (list == NULL || *list == NULL)
-	{
 		return;
-	}
+
 	temp = *list;
 	actual = temp->next;
 	while (actual != NULL)
@@ -24,22 +23,15 @@ void insertion_sort_list(listint_t **list)
 		while (change->n > actual->n)
 		{
 			if (actual->next != NULL)
-			{
 				actual->next->prev = change;
-			}
 			if (change->prev != NULL)
-			{
 				change->prev->next = actual;
-			}
-
 			change->next = actual->next;
 			actual->prev = change->prev;
 			actual->next = change;
 			change->prev = actual;
 			if (actual->prev != NULL)
-			{
 				change = actual->prev;
-			}
 			else
 			{
 				*list = actual;
